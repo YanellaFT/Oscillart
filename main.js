9ab0348e395841358762d3a987739913cc82d89f
YanellaFT <yanellaft@gmail.com>
2025-07-22 22:40:21 +0000

    part 3

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const input = document.getElementById('input');
+var amplitude = 40;
+var interval = null;
+
+//define canvas variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
 
 //create web audio api elements
 const audioCtx = new AudioContext();
@@ -36,4 +44,33 @@ gainNode.gain.value = 0;
 
 function handle() {
     frequency(input.value);
+    drawWave();
 }
+
+var counter = 0;
+function drawWave() {
+    ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height/2;
+    ctx.moveTo(x, y);
+    ctx.beginPath();
+
+    counter = 0;
+    interval - setInterval(line,20);
+}
+
+function line() {
+    freq = pitch / 10000;
+    y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    x = x + 1;
+
+    //increase counter by 1 to show how long interval has been run
+    counter++;
+
+    if(counter > 50) {
+        clearInterval(interval);
+    }
+}
+
